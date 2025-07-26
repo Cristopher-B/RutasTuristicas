9da1ec19fe1b49b71239e774459ac42c37b74a3a
SandyTanicuchi <siopolo16@gmail.com>
2025-07-26 17:49:01 -0500

    lineas de algortimo de busqueda

--- codigoRutas.py
+++ codigoRutas.py
@@ -1,4 +1,5 @@
 import re
+import heapq
 
 #Inicio de sesion.
 
@@ -13,21 +14,21 @@ def contrasenia(clave):
 def registrar_usuario():
     print("=== REGISTRO DE USUARIO ===")
     nombres = input("Nombres y Apellidos: ")
-    identificacion = input("Identificación: ")
+    identificacion = input("Identificacion: ")
     edad = input("Edad: ")
-    usuario = input("Correo electrónico: ")
+    usuario = input("Correo electronico: ")
     clave = input("Contraseña segura: ")
 
     if not contrasenia(clave):
-        print("La contraseña no es segura. Debe tener al menos una mayúscula, una minúscula y un número.")
+        print("La contraseña no es segura. Debe tener al menos una mayuscula, una minuscula y un numero.")
         return
 
     with open("usuarios.txt", "a") as archivo:
         archivo.write(f"{usuario},{clave},{nombres},{identificacion},{edad}\n")
-    print("Usuario registrado con éxito.")
+    print("Usuario registrado con exito.")
 
 def iniciar_sesion():
-    print("=== INICIO DE SESIÓN ===")
+    print("=== INICIO DE SESION ===")
     usuario = input("Correo: ")
     clave = input("Contraseña: ")
 
@@ -36,11 +37,11 @@ def iniciar_sesion():
             for linea in archivo:
                 datos = linea.strip().split(",")
                 if datos[0] == usuario and datos[1] == clave:
-                    print("Inicio de sesión exitoso.")
+                    print("Inicio de sesion exitoso.")
                     return True, usuario
         print("Credenciales incorrectas.")
         return False, None
-    #para cuando no existe el archivo.
+
     except FileNotFoundError: 
         print("Archivo de usuarios no encontrado.")
         return False, None
@@ -62,15 +63,70 @@ def leer_rutas():
     except FileNotFoundError:
         print("Archivo de rutas no encontrado.")
         return {}
+#guardar txt
+def guardar_rutas(rutas):
+    with open("rutas.txt", "w") as archivo:
+        for origen in rutas:
+            for destino in rutas[origen]:
+                distancia = rutas[origen][destino]["distancia"]
+                costo = rutas[origen][destino]["costo"]
+                archivo.write(f"{origen},{destino},{distancia},{costo}\n")
+    print("Rutas guardadas exitosamente")
+# ordenamiento burbuja 
+def ordenamiento(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range (0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista 
+#busqueda lineal
+def busqueda(lista, elemento):
+    for i, item in enumerate(lista):
+        if item == elemento:
+            return i 
+    return -1
+#revisar algoritmo dijkstra
+def dijkstra(grafo, inicio, destino):
+    distancias = {nodo:float('inf') for nodo in grafo}
+    distancias[inicio] = 0
+    cola =[(0, inicio)]
+    while cola: 
+        distancia_actual, nodo_actual = heapq.heappop(cola)
+        if distancia_actual > distancias[nodo_actual]:
+            continue
+        if nodo_actual == destino:
+            break
+        if nodo_actual in grafo:
+            for vecino, peso in grafo[nodo_actual]:
+                nueva_distancia = distancia_actual + peso
+                if nueva_distancia < distancias.get(vecino, float('inf')):
+                    distancias[vecino] = nueva_distancia
+                    heapq.heappush(cola, (nueva_distancia, vecino))
+    return distancias
 
-#Mod principal
+def encontrar_ruta(grafo, inicio, destino):
+    grafo_dijkstra ={}
+    for origen in grafo:
+        grafo_dijkstra[origen] =[]
+        for dest, datos in grafo[origen].items():
+            grafo_dijkstra[origen].append((dest, datos['costo']))
+    distancias= dijkstra(grafo_dijkstra, inicio)
+    if destino in distancias and distancias[destino] != float('inf'):
+        return distancias[destino], [inicio, destino]
+    else:
+        return float ('inf'),[]
+        
+        
+        
+
 def main():
     while True:
-        print("\n=== SISTEMA DE RUTAS TURÍSTICAS ===")
+        print("\n=== SISTEMA DE RUTAS TURISTICAS ===")
         print("1. Registro.")
-        print("2. Inicio de sesión")
+        print("2. Inicio de sesion")
         print("3. Salir")
-        op = input("Opción: ")
+        op = input("Opcion: ")
 
         if op == "1":
             registrar_usuario()
