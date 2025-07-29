7663d3d7cb70090c13f1ba873b78a5acb19af7d3
KerlyTaday <kerlytaday10@gmail.com>
2025-07-28 23:10:30 -0500

    Update codigoRutas.py

--- codigoRutas.py
+++ codigoRutas.py
@@ -116,10 +116,117 @@ def encontrar_ruta(grafo, inicio, destino):
         return distancias[destino], [inicio, destino]
     else:
         return float ('inf'),[]
-        
-        
-        
 
+#Estrucura de ARBOL
+def lugares_turisticos(arbol, nivel=0):
+    for lugares in arbol:
+        print("  "* nivel + "- " + lugares)
+        if isinstance(arbol[lugares], dict):
+            lugares_turisticos(arbol[lugares], nivel + 1)
+
+arbol_Lugares={
+    "Insular":{
+        "Galápagos":{},
+    },
+    "Costa":{
+        "Esmeraldas":{},
+        "Manta":{},
+        "Montañita":{},
+        "Salinas":{},
+        "Puerto López":{},
+    },
+    "Sierra":{
+        "Baños":{},
+        "Cuenca":{},
+        "Otavalo":{},
+        "Quito":{},
+        "Riobamba":{},
+    },
+    "Oriente":{
+        "Archidona":{},
+        "Misahuallí":{},
+        "Lago Agrio":{},
+        "Puyo":{},
+        "Tena":{},
+    }
+}
+        
+def agregar_lugar():
+    origen = input("Ciudad de origen: ")
+    destino = input("Ciudad de destini: ")
+    distancia = input("Distancia en (km): ")
+    costo= input("Precio $: ")
+
+    with open("rutas.txt", "a") as archivo:
+        archivo.write(f"{origen},{destino},{distancia},{costo}\n")
+    print("Lugar agregado con exito.")
+
+#Menu del admin
+def administrador():
+    while True:
+        print("\n ### MENÚ DE ADMINISTRADOR ###")
+        print("1. Agregar ciudad turistico.")
+        print("2. Ver lista de ciudades turisticos.")
+        print("3. Consultar una ciudad turistica.")
+        print("4. Actualizar ciudades turisticos.")
+        print("5. Eliminar ciudades turisticos.")
+        print("6. Guardar ciudades turisticos en archivo .txt.")
+        print("7. Salir.")
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            agregar_lugar()
+        elif opcion == "2":
+            rutas = leer_rutas()
+            for origen in rutas:
+                for destino in rutas[origen]:
+                    informacion = rutas[origen][destino]
+                    print(f"{origen} -> {destino} | Distancia: {informacion["distancia"]} km | Costo: ${informacion["costo"]}")
+        elif opcion == "3":
+        elif opcion == "4":
+        elif opcion == "5":
+        elif opcion == "6":
+            guardar_rutas()
+        elif opcion == "7":
+            break          
+
+#Menu del cliente       
+def cliente(user):
+    rutas = leer_rutas()
+    while True:
+         print(f"\n### MENÚ CLIENTE ({user}) ###")
+         print("1. Ver mapa de ciudades turísticos conectados.")
+         print("2. Calcular ruta mas corta.")
+         print("3. Explorar ciudades.")
+         print("4. Seleccionar ciudades a visitar.")
+         print("5. Ver lista de ciudades turísticos.")
+         print("6. Actualizar ciudades escogidos.")
+         print("7. Eliminar ciudades escogidas.")
+         print("8. Guardar la selección en archivo .txt.")
+         print("9. Salir.")        
+         opcion = input("Opción: ")
+         if opcion == "1":
+            for origen in rutas:
+                for destino in rutas[origen]:
+                    info = rutas[origen][destino]
+                    print(f"{origen} -> {destino} | Distancia: {info['distancia']} km | Costo: ${info['costo']}")
+         elif opcion == "2":
+            origen = input("Ciudad origen: ")
+            destino = input("Ciudad destino: ")
+            costo, camino = dijkstra(rutas, origen, destino)
+            if camino:
+                print("Ruta:", " -> ".join(camino))
+                print("Costo total:", costo)
+            else:
+                print("Ruta no encontrada. Intente de nuevo.")
+         elif opcion == "3":
+            lugares_turisticos(arbol_Lugares)
+         elif opcion == "4":
+         elif opcion == "5":
+         elif opcion == "6":
+         elif opcion == "7":
+         elif opcion == "8":
+         elif opcion == "9":
+            break
 def main():
     while True:
         print("\n=== SISTEMA DE RUTAS TURISTICAS ===")
@@ -134,9 +241,9 @@ def main():
             ok, usuario = iniciar_sesion()
             if ok:
                 if usuario == "admin@poli.com":
-                    #Debe ir una funcion de MENU DE ADMINISTRADOR
+                    administrador()
                 else:
-                    #Debe ir una funcion de MENU DE USUARIO
+                    cliente(usuario)
         elif op == "3":
             print("Gracias por usar nuestro sistema.")
             break
